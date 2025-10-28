4e1bcb98c70c6527d9ee01256b138fa0855118d6
SEANPNEX <ksgs1@163.com>
2025-10-28 08:58:00 -0400

    update init: Fix import

--- fintech_utils/__init__.py
+++ fintech_utils/__init__.py
@@ -2,12 +2,14 @@ from . import stats
 from . import linalg
 from . import findata
 from . import momentum
+from . import plots
 
 __all__ = [
     "stats",
     "linalg",
     "findata",
     "momentum",
+    "plots",
 ]
 
 
